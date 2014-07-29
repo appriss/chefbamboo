1353761dace1d36fd6c50ff8d1ca9258250db46c
David <dsledge@appriss.com>
2014-07-29 03:32:23 +0000

    Fixed bug in bamboo profile

--- recipes/default.rb
+++ recipes/default.rb
@@ -82,7 +82,7 @@ if node[:bamboo][:agent][:enable_ssl]
 
   # Create bamboo environment profile
   file "/etc/profile.d/bamboo.sh" do
-    content "SSL_OPTS=-Djavax.net.ssl.keyStore=#{node[:bamboo][:home]}/bamboo_server.ks -Djavax.net.ssl.keyStorePassword=#{node[:bamboo][:agent][:enable_ssl][:keystore_password]};JAVA_OPTS=$JAVA_OPTS:$SSL_OPTS; export SSL_OPTS JAVA_OPTS;"
+    content "export SSL_OPTS=\"-Djavax.net.ssl.keyStore=#{node[:bamboo][:home]}/bamboo_server.ks -Djavax.net.ssl.keyStorePassword=#{node[:bamboo][:agent][:enable_ssl][:keystore_password]}\";export JAVA_OPTS=$JAVA_OPTS:$SSL_OPTS;"
     owner "root"
     group "root"
     mode "0644"
