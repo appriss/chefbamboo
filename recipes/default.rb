6885319f6e648d4b3a1cb7d73366cd3fc885bd5c
David <dsledge@appriss.com>
2014-07-29 18:58:41 +0000

    Made a profile recipe to setup the bamboo.sh profile

--- recipes/default.rb
+++ recipes/default.rb
@@ -79,15 +79,6 @@ if node[:bamboo][:agent][:enable_ssl]
     owner node[:bamboo][:run_as]
     action :create
   end
-
-  # Create bamboo environment profile
-  file "/etc/profile.d/bamboo.sh" do
-    content "export SSL_OPTS=\"-Djavax.net.ssl.keyStore=#{node[:bamboo][:home]}/bamboo_server.ks -Djavax.net.ssl.keyStorePassword=#{node[:bamboo][:agent][:enable_ssl][:keystore_password]}\";export JAVA_OPTS=$JAVA_OPTS:$SSL_OPTS;"
-    owner "root"
-    group "root"
-    mode "0644"
-    action :create
-  end
 end
 
 # Install main config file
